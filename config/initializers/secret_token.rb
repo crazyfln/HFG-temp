78b3ebb88816a00a4b40127a3e6c99428b693b8c
crazyfln <jeff.wge@gmail.com>
2014-04-28 17:03:39 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wahaha::Application.config.secret_key_base = '6863fec6856ddd7f3a2d409f69d5e578ab784e4799f8b3869c1edcb01595ead2a79d8ac01f2d37c663ade6f729d89667fc9f4b5804cc98297159ff7d3ca152f9'
